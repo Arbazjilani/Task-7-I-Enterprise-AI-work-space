a08d83e049c2fc5043cb129f2f02132daba46de6
MD.Arbazkhan <150552640+Arbazjilani@users.noreply.github.com>
2026-08-05 16:46:39 +0530

    requment.txt

--- ai-workspace-enterprise-copilot-backend/app/main.py
+++ ai-workspace-enterprise-copilot-backend/app/main.py
@@ -46,7 +46,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://task-7-i-enterprise-ai-work-space.vercel.app/",
+        "https://task-7-i-enterprise-ai-work-space.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
